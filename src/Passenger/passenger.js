38727bfa3f513f6515c89a6512eafbc98b78ac60
Alvin Lee <alvin.nt.lee@gmail.com>
2018-03-10 13:30:56 -0800

    Added search bar to Passenger

--- src/Passenger/passenger.js
+++ src/Passenger/passenger.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import SearchBar from 'material-ui-search-bar';
 
 class Passenger extends Component {
   render() {
     return (
       <div>
-        <h1>Passenger</h1>
+        <h1>Hello, Passenger!</h1>
+        <SearchBar
+          onChange={() => console.log('onChange')}
+          onRequestSearch={() => console.log('onRequestSearch')}
+          style={{
+            margin: '0 auto',
+            maxWidth: 1800
+          }}
+        />
+        
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App/app';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Passenger from './Passenger/passenger.js';
+
+ReactDOM.render(<MuiThemeProvider> <Passenger/> </MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
